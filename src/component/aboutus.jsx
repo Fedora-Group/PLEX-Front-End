e799248064e9ad84475db881f1d89f45aee0aaa5
NeveenBeiram <neveenbeiram@yahoo.com>
2021-07-28 04:32:20 +0300

    not done yet

--- src/component/aboutus.jsx
+++ src/component/aboutus.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
+import './aboutCss.css';
 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function AboutUs() {
 
@@ -102,10 +105,19 @@ function AboutUs() {
                     {/* </div> */}
                 </div>
                 {/* style={{ 'display': 'grid', 'grid-template-columns': '1fr 1fr 1fr', 'grid-column-gap': '20px', 'grid-row-gap': '20px', 'grid-template-rows': '1fr 1fr 1fr', 'grid-template-columns': '1fr 1fr 1fr', 'margin': 'auto' }} */}
-                <div className='grid  grid-cols-3 gap-2 m-14 ' >
+                <div className='grid  grid-cols-5 gap-2 m-14 ' >
 
+                <div class="img-wrapper">
+                        <img src="https://avatars.githubusercontent.com/u/55441454?v=4" alt="Atul Prajapati" />
+                        <h2>Ibrahim Banat</h2>
+                        <ul>
+                            <li><a href="https://github.com/ibrahimBanat"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                            </a></li>
+                            <li><a href="https://www.linkedin.com/in/ibrba/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+                        </ul>
+                    </div>
 
-                    <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
+                    {/* <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
                         <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/55441454?v=4" alt='ibrahim' style={{ 'width': '200px', 'height': '200px' }} />
                         <div className='px-6 py-4'>
                             <h2 className='font-bold text-xl mb-2 text-blue-800'>Ibrahim Banat</h2>
@@ -115,7 +127,7 @@ function AboutUs() {
 
                                 {/* <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://github.com/ibrahimBanat' >GitHub</a><br />
                                 <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://www.linkedin.com/in/ibrba/' >LinkedIn</a>
-                            */}
+                            }
                                 <div className='grid grid-cols-2 gap-10'>
 
                                     <a className=' p-5' href='https://www.linkedin.com/in/ibrba/'>
@@ -129,11 +141,21 @@ function AboutUs() {
 
                             </div>
                         </div>
+                    </div> */}
+
+
+                    <div class="img-wrapper">
+                        <img src="https://avatars.githubusercontent.com/u/78357436?v=4" alt="Atul Prajapati" />
+                        <h2>Mohammad Quthama</h2>
+                        <ul>
+                            <li><a href="https://github.com/mohammad-qethama"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                            </a></li>
+                            <li><a href="https://www.linkedin.com/in/mohammad-quthama-500541184/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+                        </ul>
                     </div>
 
 
-
-                    <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
+                    {/* <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
                         <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/78357436?v=4" alt='mohammad' style={{ 'width': '200px', 'height': '200px' }} />
                         <div className='px-6 py-4'>
                             <h2 className='font-bold text-xl mb-2 text-blue-800'>Mohammad Quthama</h2>
@@ -143,7 +165,7 @@ function AboutUs() {
 
                                 {/* <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://github.com/mohammad-qethama' >GitHub</a><br />
                                 <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://www.linkedin.com/in/mohammad-quthama-500541184/' >LinkedIn</a>
-                             */}
+                             }
 
                                 <div className='grid grid-cols-2 gap-10'>
 
@@ -158,10 +180,20 @@ function AboutUs() {
 
                             </div>
                         </div>
+                    </div> */}
+
+
+                    <div class="img-wrapper">
+                        <img src="https://avatars.githubusercontent.com/u/78385348?v=4" alt="Atul Prajapati" />
+                        <h2>Neveen Beiram</h2>
+                        <ul>
+                            <li><a href="https://github.com/NeveenBeiram"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                            </a></li>
+                            <li><a href="https://www.linkedin.com/in/neveen-beiram/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+                        </ul>
                     </div>
 
-
-
+{/* 
                     <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
                         <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/78385348?v=4" alt='Neveen' style={{ 'width': '200px', 'height': '200px' }} />
                         <div className='px-6 py-4'>
@@ -172,7 +204,7 @@ function AboutUs() {
 
                                 {/* <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://github.com/NeveenBeiram' >GitHub</a><br />
                                 <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://www.linkedin.com/in/neveen-beiram/' >LinkedIn</a>
-                             */}
+                             }
                                 <div className='grid grid-cols-2 gap-10'>
 
                                     <a className=' p-5' href='https://www.linkedin.com/in/neveen-beiram/'>
@@ -186,10 +218,21 @@ function AboutUs() {
 
                             </div>
                         </div>
+                    </div> */}
+
+
+
+                    <div class="img-wrapper">
+                        <img src="https://avatars.githubusercontent.com/u/78357015?v=4" alt="Atul Prajapati" />
+                        <h2>Tamara Al-Billeh</h2>
+                        <ul>
+                            <li><a href="https://github.com/tamaraalbilleh"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                            </a></li>
+                            <li><a href="https://www.linkedin.com/in/tamaraalbilleh/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+                        </ul>
                     </div>
 
-
-
+{/* 
                     <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
                         <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/78357015?v=4" alt='tamarajr' style={{ 'width': '200px', 'height': '200px' }} />
                         <div className='px-6 py-4'>
@@ -200,7 +243,7 @@ function AboutUs() {
 
                                 {/* <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://github.com/tamaraalbilleh' >GitHub</a><br />
                                 <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://www.linkedin.com/in/tamaraalbilleh/' >LinkedIn</a>
-                            */}
+                            }
 
                                 <div className='grid grid-cols-2 gap-10'>
 
@@ -215,16 +258,29 @@ function AboutUs() {
 
                             </div>
                         </div>
+                    </div> */}
+
+
+
+
+                    <div class="img-wrapper">
+                        <img src="https://avatars.githubusercontent.com/u/78374286?v=4" alt="Atul Prajapati" />
+                        <h2>Tamara Alrashed</h2>
+                        <ul>
+                            <li><a href="https://github.com/Tamaraalrashed"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                            </a></li>
+                            <li><a href="https://www.linkedin.com/in/tamara-alrashed/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a></li>
+                        </ul>
                     </div>
 
 
-                    <div className='transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'>
-                        <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/78374286?v=4" alt='tamarasr' style={{ 'width': '200px', 'height': '200px' }} />
-                        <div className='px-6 py-4'>
-                            <h2 className='font-bold text-xl mb-2 text-blue-800'>Tamara Al-Rashed</h2>
-                            <p className='text-gray-700 text-base'>Full Stack Web Developer</p>
-                            <div className=' px-6 pt-4 pb-2 '>
-                                <p className='animate-bounce  text-red-600'>contact me </p>
+                    {/* <div className='img-wrapper transform hover: transition duration-500 hover:scale-125 m-auto max-w-sm rounded overflow-hidden shadow-lg mb-14'> */}
+                        {/* <img className='w-full m-auto' src="https://avatars.githubusercontent.com/u/78374286?v=4" alt='tamarasr' style={{ 'width': '200px', 'height': '200px' }} /> */}
+                        {/* <div className='px-6 py-4'> */}
+                            {/* <h2 className='font-bold text-xl mb-2 text-blue-800'>Tamara Al-Rashed</h2> */}
+                            {/* <p className='text-gray-700 text-base'>Full Stack Web Developer</p> */}
+                            {/* <div className=' px-6 pt-4 pb-2 '> */}
+                                {/* <p className='animate-bounce  text-red-600'>contact me </p> */}
                                 {/* <a className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://github.com/Tamaraalrashed' >GitHub</a><br /> */}
 
                                 {/* <IconButton>
@@ -232,20 +288,28 @@ function AboutUs() {
                             <a  className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' href='https://www.linkedin.com/in/tamara-alrashed/'>LinkedIn</a>
                             </LinkedInIcon>
                             </IconButton> */}
-                                <div className='grid grid-cols-2 gap-10'>
+                                {/* <ul> */}
 
-                                    <a className=' p-5' href='https://www.linkedin.com/in/tamara-alrashed/'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
-                                    </a>
+                                    {/* <div  className='grid grid-cols-2 gap-10'> */}
+                                    {/* <li> */}
 
-                                    <a className=' p-5' href='https://github.com/Tamaraalrashed'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
-                                    </a>
-                                </div>
+                                        {/* <a className=' p-5' href='https://www.linkedin.com/in/tamara-alrashed/'> */}
+                                            {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg> */}
+                                        {/* </a> */}
+                                    {/* </li> */}
 
-                            </div>
-                        </div>
-                    </div>
+                                    {/* <li> */}
+
+                                        {/* <a className=' p-5' href='https://github.com/Tamaraalrashed'> */}
+                                            {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg> */}
+                                        {/* </a> */}
+                                    {/* </li> */}
+                                    {/* </div> */}
+                                {/* </ul> */}
+
+                            {/* </div> */}
+                        {/* </div> */}
+                    {/* </div> */}
                 </div>
 
             </div>
